cc0c714ec79d13465b2009bef6927214b5f8e418
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-13 16:04:20 -0300

    feat: ensure SaveLeadController return 400 if invalid email is provided

--- src/infra/controllers/save-lead.controller.ts
+++ src/infra/controllers/save-lead.controller.ts
@@ -5,32 +5,30 @@ import { HttpRequest } from '@/shared/types'
 
 export class SaveLeadController implements ControllerInterface {
   async execute (input: HttpRequest): Promise<any> {
-    const requiredFieldsError = this.requiredFieldsValidator(input)
-    if (requiredFieldsError) {
-      return badRequest(requiredFieldsError)
-    }
-
-    const invalideEmailError = this.emailValidator(input.body.email)
-    if (invalideEmailError) {
-      return badRequest(invalideEmailError)
+    const inputError = this.inputValidator(input)
+    if (inputError) {
+      return badRequest(inputError)
     }
 
     return null
   }
 
-  private requiredFieldsValidator (input: HttpRequest): Error | undefined {
+  private inputValidator (input: HttpRequest): Error | undefined {
     const requiredFields = ['name', 'email', 'birthDate', 'phoneNumber']
     for (const field of requiredFields) {
       if (!input.body[field]) {
         return new MissingParamError(field)
       }
     }
-  }
 
-  private emailValidator (email: string): Error | undefined {
-    const regex: RegExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-    if (!regex.test(email)) {
+    const invalideEmailError = this.emailValidator(input.body.email)
+    if (!invalideEmailError) {
       return new InvalidParamError('email')
     }
+  }
+
+  private emailValidator (email: string): boolean {
+    const regex: RegExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    return regex.test(email)
   }
 }
--- src/shared/validators/email.validator.ts
+++
@@ -1,8 +0,0 @@
-import { EmailValidatorInterface } from '@/application/contracts/email-validator.interface'
-
-export class EmailValidator implements EmailValidatorInterface {
-  validate (email: string): boolean {
-    const regex: RegExp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-    return regex.test(email)
-  }
-}
