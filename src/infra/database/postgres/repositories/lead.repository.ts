a259d2deda476425e1cf71551ee26b25400a12e5
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-13 17:34:29 -0300

    refactor: change updatedAd field from Lead model to optional

---
+++ src/infra/database/postgres/repositories/lead.repository.ts
@@ -0,0 +1,18 @@
+import { SaveLeadRepositoryInterface } from '@/domain/contracts/lead-repository.interface'
+import { prismaClient } from './prisma-client'
+
+export class LeadRepository implements SaveLeadRepositoryInterface {
+  async save (input: SaveLeadRepositoryInterface.Input): Promise<void> {
+    await prismaClient.lead.create({
+      data: {
+        id: input.id,
+        name: input.name,
+        email: input.email,
+        birthDate: input.birthDate,
+        phoneNumber: input.phoneNumber,
+        status: input.status,
+        createdAt: input.createdAt
+      }
+    })
+  }
+}
