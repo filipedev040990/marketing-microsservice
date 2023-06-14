7ede352d71476307f4cf950318031882ccd60871
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-14 17:41:04 -0300

    feat: add express router adapter

---
+++ src/infra/adapters/express-route.adapter.ts
@@ -0,0 +1,14 @@
+import { ControllerInterface } from '@/application/contracts/controller.interface'
+import { HttpRequest, HttpResponse } from '@/shared/types'
+import { Request, Response } from 'express'
+
+export const expressRouteAdapter = (controller: ControllerInterface) => {
+  return async (req: Request, res: Response) => {
+    const input: HttpRequest = req.body
+    const output: HttpResponse = await controller.execute(input)
+
+    const bodyOutput = output.statusCode === 500 ? { error: output.body.message } : output.body
+
+    res.status(output.statusCode).json(bodyOutput)
+  }
+}
