59fc0f6f81f21f952093f555ffd043ef7e63e0fa
Filipe Siqueira <filipedev040990@gmail.com>
2023-06-16 11:25:47 -0300

    feat: ensure GetUnconfirmedPaymentsBot call Queue.publish with correct values

--- src/infra/bots/get-unconfirmed-payments.bot.spec.ts
+++ src/infra/bots/get-unconfirmed-payments.bot.spec.ts
@@ -38,4 +38,27 @@ describe('GetUnconfirmedPaymentsBot', () => {
 
     expect(publishNotificationInExchange.execute).toHaveBeenCalledTimes(2)
   })
+
+  test('should call Queue.publish with correct values', async () => {
+    getLeadUnconfirmedPaymentUseCase.execute.mockResolvedValueOnce([
+      {
+        name: 'anotherName',
+        email: 'anotherEmail@email.com',
+        status: 'lead',
+        createdAt: new Date('anyDate')
+      }])
+
+    await sut.execute()
+
+    expect(publishNotificationInExchange.execute).toHaveBeenCalledTimes(1)
+    expect(publishNotificationInExchange.execute).toHaveBeenCalledWith({
+      exchange: 'send_notification',
+      routingKey: 'unconfirmed_payments',
+      message: {
+        subject: 'Inscrição não concluída',
+        to: 'anotherEmail@email.com',
+        body: 'Olá, anotherName tudo bem? Observamos que você não concluiu sua matricula em nossa plataforma. Para ajuda, contacte nosso suporte pelo email suporte@email.com.br'
+      }
+    })
+  })
 })
